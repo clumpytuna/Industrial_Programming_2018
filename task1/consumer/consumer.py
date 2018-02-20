9aea3ca998760694842a6a8cfbf4a925b2c1bfb6
Clumpy Tuna <anuhinm@mail.ru>
2018-02-20 16:35:31 +0300

    Restructure

--- task1/consumer/consumer.py
+++ task1/consumer/consumer.py
@@ -2,7 +2,7 @@
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='amqp://guest:guest@localhost:5672'))
+        host='amqp://:guest@localhost:5672'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
@@ -10,7 +10,7 @@ channel.queue_declare(queue='hello')
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
+    print (" [x] Received %r" % (body,))
 
 channel.basic_consume(callback,
                       queue='hello',
