d0beb3a4e5a09e761ca29ce307186da0d3627f21
Clumpy Tuna <anuhinm@mail.ru>
2018-02-25 20:22:38 +0300

    Now docker is used

--- task2/mysite/todolist/urls.py
+++ task2/mysite/todolist/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'todolist'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:affair_id>/doer/', views.doer, name='doer'),
-    path('/adder', views.adder, name='adder')
+    path('adder/', views.adder, name='adder')
 ]
